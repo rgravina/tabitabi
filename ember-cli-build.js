b74080259f47e776cac74cd50b9d692749badf90
Robert Gravina <robert.gravina@kii.com>
2015-07-27 13:15:28 +0900

    Use flatly theme.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    'ember-cli-bootswatch': 'cyborg'
+    'ember-cli-bootswatch': 'flatly'
   });
 
   // Use `app.import` to add additional libraries to the generated
