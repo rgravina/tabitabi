3c22d36e2387eaa5b26af0cea9f484d7ad8582f5
Robert Gravina <robert.gravina@kii.com>
2015-07-31 14:06:58 +0900

    Change to be able to access isAuthenticated in beforeModel.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,17 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
     this.get("session").fetch().catch(function() {});
-  },
-
-  actions: {
-    signIn: function(provider) {
-      this.get("session").open("firebase", {provider: provider}).then(function(data) {
-        //console.log(data.currentUser);
-      });
-    },
-
-    signOut: function() {
-      this.get("session").close();
-    }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function() {
+    if (this.get("session").get("isAuthenticated")) {
+      console.log("Authenticated.")
+    };
+  },
+  actions: {
+    signIn: function(provider) {
+      this.get("session").open("firebase", {provider: provider}).then(function(data) {
+        //console.log(data.currentUser);
+      });
+    },
+    signOut: function() {
+      this.get("session").close();
+    }
+  }
 });
