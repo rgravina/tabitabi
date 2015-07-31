fd2814471cb546bc1074dcd4e3f6c4d0345fb83f
Robert Gravina <robert.gravina@kii.com>
2015-07-31 10:23:10 +0900

    Add torii adapter.

--- app/routes/application.js
+++ app/routes/application.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
 
   actions: {
     signIn: function(provider) {
-      this.get("session").open("firebase", { provider: provider}).then(function(data) {
+      this.get("session").open("firebase", {provider: provider}).then(function(data) {
         console.log(data.currentUser);
       });
     },
