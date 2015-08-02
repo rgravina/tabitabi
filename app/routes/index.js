eb19a32c2b311233040aafa5d7350b14d7995907
Robert Gravina <robert.gravina@gmail.com>
2015-08-02 16:57:15 +0900

    Added top nav on home route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,13 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
     if (this.get("session").get("isAuthenticated")) {
-      console.log("Authenticated.")
+      this.transitionTo("home");
     };
   },
   actions: {
     signIn: function(provider) {
-      this.get("session").open("firebase", {provider: provider}).then(function(data) {
-        //console.log(data.currentUser);
+      this.get("session").open("firebase", {provider: provider}).then((data) => {
+        this.transitionTo("home");
       });
     },
     signOut: function() {
